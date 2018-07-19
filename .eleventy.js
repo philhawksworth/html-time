11338257bdf8508c4f058db449c795b59076fbda
Phil Hawksworth <phil@netify.com>
2018-07-19 23:44:15 +0100

    redirects in dist

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(config) {
 
   // just pass the css we find through to the dist folder
   config.addPassthroughCopy("src/css");
+  config.addPassthroughCopy("_redirects");
 
   config.addFilter("time", require("./filters/time.js") );
   config.addFilter("zone", require("./filters/zone.js") );
