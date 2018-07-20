9735ed020d0350cde47110b371e449245811df9c
Phil Hawksworth <phil@netify.com>
2018-07-20 15:32:57 +0100

    add a buffer for build time

--- filters/time.js
+++ filters/time.js
@@ -4,6 +4,7 @@
 */
 module.exports = function(zone) {
   let now = new Date();
+  now.setTime(now.getTime() + 30000); // the build takes about 30 seconds... let's add a little buffer.
   let time = now.toLocaleString('en-US', { timeZone: zone, hour: '2-digit', minute: '2-digit' });
   return time;
 }
