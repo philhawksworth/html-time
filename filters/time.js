4d262fd0ff17cbf7778756ff30a60d8f99721cfe
Phil Hawksworth <phil@netify.com>
2018-07-21 11:58:31 +0100

    ad locale and location definitions

--- filters/time.js
+++ filters/time.js
@@ -2,9 +2,9 @@
 /*
   What's the time, Mr Wolf? (per a timezone)
 */
-module.exports = function(zone) {
+module.exports = function(data) {
+  let locale = data.locale || "en-US";
   let now = new Date();
   now.setTime(now.getTime() + 30000); // the build takes about 30 seconds... let's add a little buffer.
-  let time = now.toLocaleString('en-US', { timeZone: zone, hour: '2-digit', minute: '2-digit' });
-  return time;
+  return time = now.toLocaleString(locale, { timeZone: data.location, hour: 'numeric', minute: 'numeric'});
 }
