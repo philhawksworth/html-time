ceb921f30a776aad8b24ca038fa2634de84bd6fb
Phil Hawksworth <phil@netlify.com>
2022-09-22 13:14:24 +0100

    all via an IP lookup

--- netlify/edge-functions/time.ts
+++ netlify/edge-functions/time.ts
@@ -1,26 +1,20 @@
 import { Context } from "https://edge.netlify.com";
-import countriesAndTimezones from "https://cdn.skypack.dev/countries-and-timezones";
-import countryLocaleMap from 'https://cdn.skypack.dev/country-locale-map';
+import iplocation from 'https://cdn.skypack.dev/iplocation';
+
+// Note: rate limits!  https://github.com/Richienb/iplocation#providers
 
 
-export default async (request: Request, context: Context) => {
+export default async (context: Context) => {
 
-  const countryCode = context.geo?.country?.code;
-  const cityName = context.geo?.city;
+  const location = await iplocation(context.ip);
+  const locale = location.country.languages[0];
+  const timezone = location.country.timezone.code
 
-  context.log({context});
+  context.log({location});
 
-
-  const country = countriesAndTimezones.getCountry(countryCode);
-  context.log({country});
-  const locale = countryLocaleMap.getCountryByAlpha2(country.id);
-  const browserlocale = locale.default_locale.replace("_", "-");
-  context.log({locale});
-
-  
   // Generate a formatted time string
   const now = new Date();
-  const time = now.toLocaleString(browserlocale, { timeZone: country.timezones[0], hour: 'numeric', minute: 'numeric'}); 
+  const time = now.toLocaleString(locale, { timeZone: timezone, hour: 'numeric', minute: 'numeric'}); 
 
   // Get the page content
   const response = await context.next();
@@ -28,7 +22,7 @@ export default async (request: Request, context: Context) => {
   
   // Replace the content
   const regex = /CURRENT_TIME/i;
-  const updatedPage = page.replace(regex, JSON.stringify(context));
+  const updatedPage = page.replace(regex, time);
   return new Response(updatedPage, response);
 
 };
